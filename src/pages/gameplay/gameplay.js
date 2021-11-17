9adf362268a0fbd0ed22d33c48723be1e68b7834
buidung-coder <buidung314@gmail.com>
2021-11-17 17:43:39 +0700

    feat:subpage gameplay

--- src/pages/gameplay/gameplay.js
+++ src/pages/gameplay/gameplay.js
@@ -1,30 +1,50 @@
 import React from 'react'
+import '../../css/gameplay.scss'
+import urlConstant from '../../urlConstant'
 
 export default function Gameplay() {
     return (
         <div className='gameplay-body'>
-           <div className='sky'>
-                <div className='sky-top'>
+            <div className='sky'>
+                <div className='sky-top container'>
                     <div className='gameplay-selection'>
-
+                        <div className='col drop'>
+                            <img className="gameplay__selection-img" src={urlConstant.imageGamePlay.drop} alt="" />
+                        </div>
+                        <div className='col nest'>
+                            <img className="gameplay__selection-img" src={urlConstant.imageGamePlay.nest} alt="" />
+                        </div>
+                        <div className='col-6 rune'>
+                            <img className="gameplay__rune-img" src={urlConstant.imageGamePlay.rune} alt="" />
+                            <img className="gameplay__boonmoon-img" src={urlConstant.imageGamePlay.boonMoon} alt="" />
+                        </div>
+                        <div className='col scout'>
+                            <img className="gameplay__selection-img" src={urlConstant.imageGamePlay.scout} alt="" />
+                        </div>
+                        <div className='col metaVerse'>
+                            <img className="gameplay__selection-img" src={urlConstant.imageGamePlay.metaVerse} alt="" />
+                        </div>
                     </div>
                     <div className='gameplay-logo'>
-                    <img className="gameplay__logo-img" src='' alt="" />
+                        <img className="gameplay__logo-img" src={urlConstant.image.eluLogo} alt="" />
+                    </div>
+                    <div className='gameplay-cloud'>
+
                     </div>
                 </div>
                 <div className='sky-center'>
-                    
+
                 </div>
                 <div className='sky-bottom'>
-                    
-                </div>
-           </div>
-           <div className='ocean'>
 
-           </div>
-           <div className='land'>
-            
-           </div>
+                </div>
+            </div>
+            <div className='ocean'>
+
+            </div>
+            <div className='land'>
+
+            </div>
         </div>
     )
 }
