b23428f0b4d238d91c28fa89c225a33435ed8b82
phamquangsang661 <phamquangsang661@gmail.com>
2021-11-20 17:37:09 +0700

    feat: island can zoom in zoom out

--- src/pages/gameplay/skyCenter.js
+++ src/pages/gameplay/skyCenter.js
@@ -16,16 +16,17 @@ const img_urls = [
         url: 'https://storage.googleapis.com/laboon-img-storage/play-elu/gameplay/island.webp'
     },
     {
-        url: 'https://storage.googleapis.com/laboon-img-storage/play-elu/gameplay/islandBlank1.webp'
+        url: 'https://storage.googleapis.com/laboon-img-storage/play-elu/gameplay/islandBlank2.webp'
     },
     {
-        url: 'https://storage.googleapis.com/laboon-img-storage/play-elu/gameplay/islanBlank3.webp'
+        url: 'https://storage.googleapis.com/laboon-img-storage/play-elu/gameplay/islandBlank1.webp'
     },
     {
         url: 'https://storage.googleapis.com/laboon-img-storage/play-elu/gameplay/islandBlank2.webp'
     }
 ]
 const calcZIndex = (deg) => {
+    deg = deg % 360
     let zIndex;
     if (deg >= 0 && deg <= 60) {
         if (deg >= 30)
@@ -51,13 +52,28 @@ const calcZIndex = (deg) => {
         return zIndex
     }
 
-    if(deg<=240){
-        if(deg>=210)
-            zIndex =3
+    if (deg <= 240) {
+        if (deg >= 210)
+            zIndex = 3
         else
-        zIndex = 4
+            zIndex = 4
         return zIndex
     }
+    if (deg <= 300) {
+        if (deg >= 270)
+            zIndex = 2
+        else
+            zIndex = 3
+        return zIndex
+    }
+    if (deg <= 360) {
+        if (deg >= 330)
+            zIndex = 1
+        else
+            zIndex = 2
+        return zIndex
+    }
+
 }
 export default function SkyCenter() {
     const [widthW, heightW] = useWindowSize()
@@ -66,7 +82,8 @@ export default function SkyCenter() {
         transformOfIsland: `rotateX(270deg) rotateY(${key * 60}deg)`,
         rotateZOfCircle: key * 60,
         rotateYOfIsland: key * 60,
-        zIndex: 1
+        zIndex: 1,
+        scale: 1
     }))
 
     const bind = useGesture({
@@ -74,6 +91,7 @@ export default function SkyCenter() {
             let focus = -30
             // console.log(state.direction)
             // console.log(state.xy)
+
             if (state.direction[0] == -1) {
                 focus = 30
             }
@@ -81,6 +99,14 @@ export default function SkyCenter() {
             set.start((key) => {
                 let rotateZAfter = islands[key].rotateZOfCircle.get() + focus
                 let rotateYAfter = islands[key].rotateYOfIsland.get() + focus
+                let zIndex = calcZIndex(Math.abs(rotateZAfter))
+                let scale = 1
+                if (zIndex == 4)
+                    scale = 1.5
+                if (zIndex == 2)
+                    scale = 0.5
+                if (zIndex == 1)
+                    scale = 0.25
                 // rotateZAfter = rotateZAfter > 0 ? rotateZAfter : 360 + rotateZAfter
                 // rotateYAfter=rotateYAfter > 0 ? rotateYAfter : 360 + rotateYAfter
                 return {
@@ -88,14 +114,15 @@ export default function SkyCenter() {
                     transformOfIsland: 'rotateX(270deg) rotateY(' + rotateYAfter + 'deg)',
                     rotateZOfCircle: rotateZAfter,
                     rotateYOfIsland: rotateYAfter,
-                    zIndex: (Math.abs(rotateZAfter) % 360 >= 90) && (Math.abs(rotateZAfter) % 360 <= 270) ? 2 : 1
+                    zIndex,
+                    scale
                 }
             })
         }
     })
 
 
-    const renderIsland = islands.map(({ transformOfCircle, transformOfIsland, zIndex }, key) => {
+    const renderIsland = islands.map(({ transformOfCircle, transformOfIsland, zIndex, scale }, key) => {
         return (
             <animated.div className='sky-center__islands' style={{
                 transform: transformOfCircle,
@@ -109,6 +136,7 @@ export default function SkyCenter() {
                         background: `url("${img_urls[key].url}") no-repeat center / contain`,
                         height: "40vh",
                         width: "40vh",
+                        scale
                     }}></animated.div>
             </animated.div>)
     })
