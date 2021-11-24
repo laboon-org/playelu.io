0e3f3dcfaf811bd07cdfde72b7b163fd43d91d57
buidung <buidung314@gmail.com>
2021-11-24 17:37:18 +0700

    fix:cursor

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,25 +26,25 @@ export default function Header() {
                 <li className='playelu__menu-element' style={{ marginRight: '5px' }}>
                     <a href="https://bit.ly/3nvuPP3" target='_blank'>
                         <img src={urlConstant.image.playeluHeader2} alt="" />
-                        <h4>ROADMAP</h4>
+                        <h4 style={{ animation: 'shine 2.5s infinite linear' }}>ROADMAP</h4>
                     </a>
                 </li>
                 <li className='playelu__menu-element' style={{ marginRight: 'auto' }}>
                     <a href='https://bit.ly/3HIpdcy' target='_blank'>
                         <img src={urlConstant.image.playeluHeader2} alt="" />
-                        <h4>TOKENOMIC</h4>
+                        <h4 style={{ animation: 'shine 1.5s infinite linear' }}>TOKENOMIC</h4>
                     </a>
                 </li>
 
                 <li className='playelu__menu-element' style={{ marginRight: '5px' }}>
                     <Link to='/gameplay'>
                         <img src={urlConstant.image.playeluHeader2} alt="" />
-                        <h4 style={{ color: '#fabb1a' }}>GAMEPLAY</h4>
+                        <h4 style={{ color: '#fabb1a', animation: 'shine 1s infinite linear' }}>GAMEPLAY</h4>
                     </Link>
                 </li>
                 <li className='playelu__menu-element' onClick={onPressMenu}>
                     <img src={urlConstant.image.playeluHeader2} alt="" />
-                    <h4 >LITEPAPER</h4>
+                    <h4 style={{ animation: 'shine 2s infinite linear' }}>LITEPAPER</h4>
                 </li>
             </ul>
 
