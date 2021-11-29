5a3bb1c75fa6a0e98938fbd28bf9a26d21695a54
phamquangsang661 <phamquangsang661@gmail.com>
2021-11-29 17:56:33 +0700

    feat: bottom cloud

--- src/pages/banner/backgrounDecor/BackgroundBottom.js
+++ src/pages/banner/backgrounDecor/BackgroundBottom.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import urlConstant from "../../../urlConstant";
-
+import cloudscss from './clouds.scss'
 
 export default function CloudBottom() {
-
+    let clouds = []
+    for (let i = 0; i < 100; i++) {
+        clouds.push((<div key={i}></div>))
+    }
     return (
         <div className="background-bottom">
-            <img className="background__bottom-img" src={urlConstant.image.backgroundBottom} alt="" />
+            <div className="background-bottom-cloud">
+            {clouds}
+            </div>
+         
+            {/* <img className="background__bottom-img" src={urlConstant.image.backgroundBottom} alt="" /> */}
         </div>
     )
 }
