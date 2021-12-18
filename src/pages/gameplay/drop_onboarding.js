d4952412cce83f85a48cb05eef5a57c2f9de3cf0
buidung <buidung314@gmail.com>
2021-12-18 17:49:28 +0700

    fix:button drop and onboarding drop

--- src/pages/gameplay/drop_onboarding.js
+++ src/pages/gameplay/drop_onboarding.js
@@ -10,9 +10,9 @@ import OnBoardingTip from '../../components/onBoarding/OnBoarding_tip'
 export default function Drop_onboarding(props) {
     const { onPressHideEluDrop } = props
     return (
-        <div className="sky-center_onBoarding" >
+        <div className="sky-center_onBoarding">
             <div className='onBoarding-frame'>
-                <div className='onBoarding__title' id='onboarding_drop-center'>
+                <div className='onBoarding__title'>
                     <div className='onBoarding__title-content'>
                         <img src={urlConstant.imageGamePlay.popupDrop.boardTitle} alt='' />
                         <div className='onBoarding__title-text'>
