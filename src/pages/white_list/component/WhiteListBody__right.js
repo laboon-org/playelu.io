cbec5a03f98fc321b4aa973e1aa164f25cdabe9b
phamquangsang661 <phamquangsang661@gmail.com>
2022-01-22 16:00:08 +0700

    feat: check time to open page

--- src/pages/white_list/component/WhiteListBody__right.js
+++ src/pages/white_list/component/WhiteListBody__right.js
@@ -1,24 +1,57 @@
 import React, { useState } from 'react'
-import LoginProcess from './LoginProcess'
+import moment from 'moment'
 
+import LoginProcess from './LoginProcess'
+import messageStorage from '../../../module/messageStorage'
+import WhiteListComingSoon from './modal/WhiteList_comingSoon'
+
+
+const checkCountdown = () => {
+    const CONFIG = messageStorage.getInstance().getMessage('config')
+    let countdown = false
+    let countdownTime = ''
+    if (CONFIG['Round Setting'] != null) {
+        const data = CONFIG['Round Setting'].data
+
+        const index = data.findIndex((v, i, obj) => {
+            if (moment(v.Start).add(1, 'days').isAfter(moment())) {
+                return true
+            }
+        })
+
+        if (index != -1) {
+            countdown = true
+            countdownTime = moment(data[index].Start).format('DD-MM-YYYY [00:00]')
+        }
+    }
+    console.log(countdownTime)
+    if (countdown) {
+        return countdownTime
+    } else {
+        return ''
+    }
+}
 const WhiteListRegistration = React.lazy(() => import('./WhiteList_Registration'))
 export default function WhiteListBody__right(props) {
-
     const { showModalNotFound } = props
-
     const [whiteListShow, setWhiteListShow] = useState(false)
+    const [commingSoonTime, setCommingSoon] = useState(checkCountdown())
     const showWhiteList = () => {
         setWhiteListShow(true)
     }
+    console.log('STATE:',commingSoonTime)
     return (
         <div className='white-list__body--right'>
+
             {
-                !whiteListShow ?
-                    <LoginProcess
-                        showWhiteList={showWhiteList}
-                        showModalNotFound={showModalNotFound}
-                    /> :
-                    <WhiteListRegistration />
+                commingSoonTime == ''
+                    ? (!whiteListShow ?
+                        (<LoginProcess
+                            showWhiteList={showWhiteList}
+                            showModalNotFound={showModalNotFound}
+                        />) :
+                        (<WhiteListRegistration />))
+                    : (<WhiteListComingSoon dateProp={commingSoonTime} />)
             }
         </div>
     )
